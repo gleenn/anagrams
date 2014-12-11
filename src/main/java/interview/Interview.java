4e22ab4ac7d8c40c8947674f138d1189ccc7e78f
Glenn <gleenn@gmail.com>
2014-12-12 02:04:07 +0900

    working simple regex matcher with ? only

--- src/main/java/interview/Interview.java
+++ src/main/java/interview/Interview.java
@@ -1,7 +1,29 @@
 package interview;
 
+import java.util.LinkedList;
+
+import static java.util.Arrays.asList;
+
 public class Interview {
-    public static int add(int a, int b) {
-        return a + b;
+    public static boolean matches(String needle, String hayStack) {
+        String[] parts = needle.split("\\?");
+        boolean endsWithQ = hayStack.endsWith("?");
+        return matches(new LinkedList<String>(asList(parts)), hayStack, endsWithQ);
+    }
+
+    private static boolean matches(LinkedList<String> parts, String hayStack, boolean endsWithQ) {
+        String part = parts.pop();
+        int length = part.length();
+
+        if(hayStack.startsWith(part)) {
+            return parts.isEmpty() || matches(parts, hayStack.substring(0, length), endsWithQ);
+        }
+
+        String skipLast = part.substring(0, Math.max(length - 2, 0));
+        if(hayStack.startsWith(skipLast)) {
+            return parts.isEmpty() && endsWithQ || matches(parts, skipLast, endsWithQ);
+        }
+
+        return false;
     }
 }
--- src/test/java/interview/InterviewTest.java
+++ src/test/java/interview/InterviewTest.java
@@ -4,9 +4,26 @@ import org.junit.Test;
 
 import static org.fest.assertions.Assertions.assertThat;
 
+
 public class InterviewTest {
     @Test
-    public void testAdd() {
-        assertThat(Interview.add(1, 2)).isEqualTo(3);
+    public void testMatches() {
+        assertThat(Interview.matches("", "a")).isTrue();
+        assertThat(Interview.matches("a", "a")).isTrue();
+        assertThat(Interview.matches("ab?", "a")).isTrue();
+        assertThat(Interview.matches("ab?a?", "aba")).isTrue();
+        assertThat(Interview.matches("ab?a?", "aa")).isTrue();
+        assertThat(Interview.matches("ab?a?", "a")).isTrue();
+        assertThat(Interview.matches("ab?a?a", "abaa")).isTrue();
+        assertThat(Interview.matches("ab?a?a", "aa")).isTrue();
+        assertThat(Interview.matches("ab?a?a", "aaa")).isTrue();
+
+        assertThat(Interview.matches("aa", "a")).isFalse();
+    }
+
+    @Test
+    public void foo() {
+        String[] parts = "aa".split("a");
+        assertThat(parts.length == 0);
     }
 }
