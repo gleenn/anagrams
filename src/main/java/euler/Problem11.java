aaa6a2b24d8e99f9aa1e5bed870b81bffa5e2b4a
Glenn <gleenn@gmail.com>
2015-01-12 17:09:46 +0900

    Java8'd part of problem 11

--- src/main/java/euler/Problem11.java
+++ src/main/java/euler/Problem11.java
@@ -2,8 +2,11 @@ package euler;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.IntBinaryOperator;
 
 import static java.lang.Math.max;
+import static java.util.stream.IntStream.of;
+import static java.util.stream.IntStream.range;
 
 public class Problem11 {
     public static int findLargestProduct(String rawDataz) {
@@ -17,8 +20,8 @@ public class Problem11 {
         }
 
         int bestProduct = 0;
-        for(int j=0; j<dataz.size()-3; j++) {
-            for(int i=0; i<dataz.get(0).size()-3; i++) {
+        for (int j = 0; j < dataz.size() - 3; j++) {
+            for (int i = 0; i < dataz.get(0).size() - 3; i++) {
                 bestProduct = max(check(i, j, 4, dataz), bestProduct);
             }
         }
@@ -26,16 +29,13 @@ public class Problem11 {
     }
 
     public static int check(int x, int y, int length, List<List<Integer>> dataz) {
-        int productRight = 1;
-        int productDown = 1;
-        int productDownRight = 1;
-        int productDownLeft = 1;
-        for(int i=0; i<length; i++) {
-            productRight *= dataz.get(y).get(x+i);
-            productDown *= dataz.get(y+i).get(x);
-            productDownRight *= dataz.get(y+i).get(x+i);
-            productDownLeft *= dataz.get(y+i).get(x-i+3);
-        }
-        return max(max(max(productRight, productDown), productDownRight), productDownLeft);
+        IntBinaryOperator product = (left, right) -> left * right;
+
+        return of(
+                range(0, length).map(i -> dataz.get(y).get(x + i)).reduce(1, product),
+                range(0, length).map(i -> dataz.get(y + i).get(x)).reduce(1, product),
+                range(0, length).map(i -> dataz.get(y + i).get(x+i)).reduce(1, product),
+                range(0, length).map(i -> dataz.get(y + i).get(x-i+length-1)).reduce(1, product)
+        ).max().getAsInt();
     }
 }
